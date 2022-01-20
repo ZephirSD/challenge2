4282282c1277ff2a2c12b24bdd667af67e6f4c6b
ZephirSD <siegfrieddallery@gmail.com>
2022-01-20 09:39:26 +0400

    Réalisation de l'interface & de la class Choix

---
+++ lib/choix.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Choix {
+  String questions;
+  String choix1;
+  String choix2;
+  Choix(this.questions, this.choix1, this.choix2);
+}
---
+++ lib/liste.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'choix.dart';
+
+class Base {
+  int numberChoix = 0;
+  List<Choix> question = [
+    Choix(
+        "Vous venez de crevez un pneu à St André. Vous n'avez pas de téléphone vous décidez de faire du stop. Une ford fiesta rouge s'arrête et le conducteur vous demande si vous voulez qu'il vous dépanne.",
+        "Vous lui remerciez et vous montez dans la voiture",
+        "Vous lui demandez s'il n'est pas un meurtrier avant !"),
+    Choix(
+        "Il acquiesce de la tête sans faire attention à la question.",
+        "Au moins il est honnête. Vous montez !",
+        "Attends, mais je sais comment changer un pneu voyons !"),
+    Choix(
+        "Lorsqu'il commence à conduire, il vous demande d'ouvrir la boite à gant. À l’intérieur, vous trouverez un couteau ensanglanté, deux doigts coupés et un CD de T-Matt.",
+        "J'adore T-Matt, je lui donne le CD.",
+        " C'est lui ou moi, je prends le couteau et je le poignarde."),
+    Choix("Woaw ! Quelle évasion !", "Recommencer", ""),
+    Choix(
+        "En traversant la route du littoral, vous réfléchissez à la sagesse douteuse de poignarder quelqu’un pendant qu’il conduit une voiture dans laquelle vous êtes.",
+        "Recommencer",
+        ""),
+    Choix(
+        "Vous vous faites un bon dalon et vous chantez le dernier son de T-matt ensemble. Il vous dépose à Cambaie et il vous demande si vous connaissez un bon endroit pour jeter un corps.",
+        "Recommencer",
+        ""),
+  ];
+  String getQuestionsChoix() {
+    return question[numberChoix].questions;
+  }
+
+  String getChoix1() {
+    return question[numberChoix].choix1;
+  }
+
+  String getChoix2() {
+    return question[numberChoix].choix2;
+  }
+}
